e63ba8c57a28cffb87a439f16cd5e54397e597ae
Jake Whatley <jacob.whatley91@gmail.com>
2013-11-14 17:39:50 -0500

    jfltevzw: Device bringup
    Change-Id: Idba7ddacc8e6437cfd0aaac05e4c717da2231400
    Signed-off-by: Jake Whatley <jacob.whatley91@gmail.com>

--- omni_jfltevzw.mk
+++ omni_jfltevzw.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2013 OmniROM Project
+# Copyright (C) 2012 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,25 +12,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
- 
-#
-# This file is the build configuration for a full Android
-# build for maguro hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps). Except for a few implementation
-# details, it only fundamentally contains two inherit-product
-# lines, full and maguro, hence its name.
-#
 
+# Grab needed APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit from those products. Most specific first.
+# Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from jfltevzw device
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# This is where we'd set a backup provider if we had one
+#$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/samsung/jfltevzw/device.mk)
 
-# Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_jfltevzw
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := omni_jfltevzw
 PRODUCT_DEVICE := jfltevzw
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
