498876329d3070ca9490ba7c59a522892edf0c48
Jake Whatley <jacob.whatley91@gmail.com>
2013-12-12 21:39:42 -0500

    jfltevzw: update to the new APNs configuration
    Updates to the new APNs configuration per https://gerrit.omnirom.org/#/c/3610/
    
    Change-Id: Idd1cf6f53b07a71cb0f34b293bf2fa0ee1801c9b
    Signed-off-by: Jake Whatley <jacob.whatley91@gmail.com>

--- omni_jfltevzw.mk
+++ omni_jfltevzw.mk
@@ -13,8 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Grab needed APNs
-$(call inherit-product, vendor/omni/config/cdma.mk)
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
